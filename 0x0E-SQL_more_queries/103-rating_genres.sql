01a7c7cfdcb41a4a95f4ca3bb4fa75cf52c5cd3d
SolomonOnwueyikpe <solofamily2003@yahoo.com>
2024-01-17 16:56:11 -0800

    sql

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,10 @@
---  The database name will be passed as an argument of the mysql command
-SELECT tg.name, SUM(tr.rate) AS rating FROM tv_show_genres tgs
-JOIN tv_genres tg
-ON tg.id = tgs.genre_id
-JOIN tv_show_ratings tr
-ON tr.show_id = tgs.show_id
-GROUP BY  tg.name DESC ORDER BY RATING DESC;
+-- The database name will be passed as an argument of the mysql command
+
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings
+ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_genres.id
+ORDER BY rating DESC;
